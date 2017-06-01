a3887d279e02c4a9d9b341bd777d4a2fcc2c38b6
zhiayang <zhiayang@gmail.com>
2017-06-01 23:38:07 +0800

    add framework/boilerplate for own vector/matrix lib for when we eventually get tired of stupid vector slowdown caused by glm

---
+++ source/include/lx.h
@@ -0,0 +1,32 @@
+// lx.h
+// Copyright (c) 2014 - 2016, zhiayang@gmail.com
+// Licensed under the Apache License Version 2.0.
+
+// Linear Algebra Library
+
+#pragma once
+
+#include <stdint.h>
+#include <stddef.h>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
